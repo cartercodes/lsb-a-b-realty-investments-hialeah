76704653a56488702ed698aecf10ede784bf103c
Dispatch <dispatch@cartercodes.dev>
2026-04-04 21:06:37 -0400

    Site deployment

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/lsb-a-b-realty-investments-hialeah',
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
